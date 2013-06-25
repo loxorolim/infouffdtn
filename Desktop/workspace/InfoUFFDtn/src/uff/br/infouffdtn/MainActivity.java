084b66dbc76d54e428e689cc4249f0dc89185e76
loxorolim <loxorolim@gmail.com>
2013-06-25 16:31:24 -0300

    Update MainActivity.java

--- Desktop/workspace/InfoUFFDtn/src/uff/br/infouffdtn/MainActivity.java
+++ Desktop/workspace/InfoUFFDtn/src/uff/br/infouffdtn/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
 	
     private InfoService mService = null;
     private boolean mBound = false;
-    //Teste do git numero 2!!!!!!!!
+    //Teste do git numero 3 agora é o pull!!!!!!!!
     //private EditText mTextEid = null;
     //private TextView mResult = null;
     private TextView editText;
